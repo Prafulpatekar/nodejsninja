9b0af1f06f418a71fae7605cebbde15bf53ccf55
Praful Patekar <prafulpatekar23@gmail.com>
2023-03-12 21:42:18 +0300

    Express App Routing Redirect 404 Page

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ const app = express();
 app.listen(3001);
 
 app.get('/',(req,res)=>{
-    res.send('<h3>HELEHNDODJKDLJ</h3>')
-})
+    res.sendFile('./views/index.html',{root:__dirname});
+});
 
+// Routing
+app.get('/about',(req,res)=>{
+    // res.send('<h3>About Page</h3>')
+    res.sendFile('./views/about.html',{root:__dirname});
+});
+
+// redirect
+app.get('/about-us',(req,res)=>{
+    // res.send('<h3>About Page</h3>')
+    res.redirect('/about')
+});
+
+// 404 Page
+app.use((req,res)=>{
+    res.sendFile('./views/404.html',{root:__dirname})
+});
