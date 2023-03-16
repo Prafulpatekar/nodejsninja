32d998270be66385567d0de4d50e25978c750ea3
Praful <prafulpatekar23@gmail.com>
2023-03-16 19:55:29 +0530

    Mongodb-Atlas, schema, model

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
+// Connecting DB
+const db = "mongodb+srv://prafulcoder:Coder.2000$23@nodejsninja.be8rcgc.mongodb.net/nodejstuts?retryWrites=true&w=majority"; 
+mongoose.connect(db)
+.then((results)=>{
+    console.log("Connected To DB")
+    // Listen server req
+    app.listen(3001);
+}).catch((err)=>{
+    console.log(err)
+});
 //Express app
 const app = express();
 
-// Listen server req
-app.listen(3001);
+// Register view engine
+app.set('view engine','ejs');
+
+
+
+// Middleware for static files
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
 
 app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html',{root:__dirname});
+    res.render('index');
 });
 
 // Routing
 app.get('/about',(req,res)=>{
     // res.send('<h3>About Page</h3>')
-    res.sendFile('./views/about.html',{root:__dirname});
+    res.render('about');
 });
 
 // redirect
-app.get('/about-us',(req,res)=>{
+app.get('/create',(req,res)=>{
     // res.send('<h3>About Page</h3>')
-    res.redirect('/about')
+    res.render('create');
 });
 
 // 404 Page
 app.use((req,res)=>{
-    res.sendFile('./views/404.html',{root:__dirname})
+    res.render('404')
 });
