3f789b683b4d8548c6ad7a2a0bfb8b450e030d1c
Praful <prafulpatekar23@gmail.com>
2023-03-21 18:46:29 +0530

    watch function and function approach

--- files.js
+++ files.js
@@ -1,40 +1,35 @@
 const fs = require('fs');
 
-// Reading
+const errHandler = err => console.log(err);
+const dataHandler = data => console.log(data.toString());
 
-fs.readFile(`${__dirname}/docs/blog2.txt`,(err,data)=>{
-    if(err){
-        console.log(err)
-    }
-    console.log(data.toString())
+// Watching file for any change
+fs.watch(`${__dirname}/docs/blog2.txt`,(err,data)=>{
+    if(err) errHandler(err);
+    dataHandler(data);
 })
 
 // Writting
-
-fs.writeFile(`${__dirname}/docs/blog2.txt`,'HELLO WORLD!!👌👌',()=>{
+fs.writeFile(`${__dirname}/docs/blog2.txt`,'HEsLLO WORLDhh!!👌👌',()=>{
     console.log("File Done!")
+    // Reading
     fs.readFile(`${__dirname}/docs/blog2.txt`,(err,data)=>{
-        if(err){
-            console.log(err)
-        }
-        console.log(data.toString())
-    })
-})
+        if(err) errHandler(err);
+        dataHandler(data);
+    });
+    
+});
 
 // Dirs
 if(!fs.existsSync(`${__dirname}/static`)){
     fs.mkdir(`${__dirname}/static`,(err)=>{
-        if(err){
-            console.log(err)
-        }
+        if(err) errHandler(err);
         console.log("CREATED")
     })
 }else{
     console.log("Alreay exist")
     fs.rmdir(`${__dirname}/static`,(err)=>{
-        if(err){
-            console.log(err)
-        }
+        if(err) errHandler(err);
         console.log("Deleted Now")
     })
 }
@@ -43,9 +38,7 @@ if(!fs.existsSync(`${__dirname}/static`)){
 // Deleting
 if (fs.existsSync(`${__dirname}/docs/blog.txt`)){
     fs.unlink(`${__dirname}/docs/blog.txt`,(err)=>{
-        if (err){
-            console.log(err);
-        }
+        if(err) errHandler(err);
         console.log("File Deleted!!")
     })
 }
